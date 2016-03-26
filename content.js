ba48b8104e90dd68a6d1fdecc6c440270e381676
Ford Hurley <ford.hurley@gmail.com>
2016-03-26 10:30:16 -0400

    Make the link work, and look pretty good too

--- content.js
+++ content.js
@@ -6,11 +6,26 @@ if (window.location.href.indexOf("/blame/") !== -1) {
 }
 
 function addLinks() {
-    var infos = window.document.getElementsByClassName("blame-commit-info");
-    Array.prototype.forEach.call(infos, function(info, index) {
-      var link = document.createElement("a");
-      link.href = window.location.href + "#L" + index;
-      link.innerHTML = "test";
-      info.appendChild(link);
-    });
-  }
+  var commits = window.document.getElementsByClassName("blame-commit");
+  Array.prototype.forEach.call(commits, function(commit) {
+    var info = commit.getElementsByClassName("blame-commit-info")[0];
+    var shaLink = info.getElementsByClassName('blame-sha')[0];
+    var sha = shaLink.textContent
+    var line = commit.nextElementSibling
+    var lineNum = line.getElementsByClassName('blob-num')[0].id;
+    var link = document.createElement("a");
+    link.href = blameLink(sha, lineNum);
+    link.innerHTML = 'blame parent';
+    link.style.float = 'right';
+    link.style.clear = 'right';
+    link.style.fontSize = '10px';
+    info.insertBefore(link, info.getElementsByClassName('blame-commit-title')[0]);
+  });
+}
+
+function blameLink(sha, lineNum) {
+  var parts = window.location.href.split('/');
+  parts[6] = sha+'^';
+  parts[parts.length-1] = parts[parts.length-1].split('#')[0] + '#' + lineNum
+  return parts.join('/');
+}
